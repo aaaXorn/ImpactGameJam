96a2336ce097029786d8efc3e5285350c301f4c4
aaaXorn <lcrs.rp@gmail.com>
2022-11-08 11:01:24 -0300

    Basic outline toggle script (untested)
    Will be activated through a raycast that targets a single specific layer, which will not interact with anything else.

---
+++ MysteryGame/Assets/Scripts/Player/Pickups/OutlineHitbox.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pickups
+{
+    public class OutlineHitbox : Monobehaviour
+    {
+        [SerializeField] Outline outline;
+
+        //if the active coroutine is running
+        bool _activeIsRunning;
+        //current coroutine time
+        float _activeTime;
+        [Tooltip("Total outline active time.")]
+        [SerializeField] float _totalActiveTime;
+
+        public void AimedAt()
+        {
+            //resets the timer
+            _activeTime = _totalActiveTime;
+            //if the coroutine isn't running, start it
+            if(!_activeIsRunning) StartCoroutine("ActiveTimer");
+        }
+
+        private IEnumerator ActiveTimer()
+        {
+            _activeIsRunning = true;
+            //enables the outline
+            outline.enabled = true;
+
+            //timer
+            while(_activeTime >= 0f)
+            {
+                _activeTime -= 0.1f;
+
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            //disables the outline
+            outline.enabled = false;
+        }
+    }
+}
